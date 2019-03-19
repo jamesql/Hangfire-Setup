bcbcfbd921c2c39996c088ef0c5d871eba7f0f5e
James <42484571+smitteh1@users.noreply.github.com>
2019-03-19 18:37:19 -0400

    Added Methods Class

---
+++ Hangfire2/Methods.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hangfire2
+{
+    public class Methods
+    {
+
+        public void Log(string line)
+        {
+            Console.WriteLine(line);
+        }
+
+    }
+}
